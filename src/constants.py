27c950bfede6b1eb66b4f9e3779481129ffcd590
rxmrsd <morisada_keisuke@retail-ai.jp>
2024-08-14 11:36:07 +0900

    initialize

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+"""constants"""
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NEO4J_URI = os.environ["NEO4J_URI"]
+NEO4J_USERNAME = os.environ["NEO4J_USERNAME"]
+NEO4J_PASSWORD = os.environ["NEO4J_PASSWORD"]
